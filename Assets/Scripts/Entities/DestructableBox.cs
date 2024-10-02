14dfb71656066df79ceba754e21fb931f4ebc88e
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-02 10:03:01 -0300

    Create objects.

---
+++ Assets/Scripts/Entities/DestructableBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    /// <summary>
+    /// A target for the player to shoot.
+    /// </summary>
+    public class DestructableBox : TargetableObject
+    {
+        public override void OnTarget()
+        {
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/TargetableObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    /// <summary>
+    /// An object that the player can target, whether to fire upon, or interact with.
+    /// </summary>
+    public abstract class TargetableObject : MonoBehaviour
+    {
+        public abstract void OnTarget();
+    }
+}
