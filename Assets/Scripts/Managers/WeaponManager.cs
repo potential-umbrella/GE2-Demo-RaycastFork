631a49261f7e5dd3d8ba076ca519e63acddebc68
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2024-10-16 15:10:32 -0300

    Turn cubes red!
    too lazy to split commits.
    
    Co-Authored-By: Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>

---
+++ Assets/Scripts/Managers/WeaponManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    internal class WeaponManager : MonoBehaviour
+    {
+        // camera refs.
+        [SerializeField] CameraManager camMan;
+        Camera playerCam;
+
+        [SerializeField] float defaultDistance = 10;
+        [SerializeField] int raycastInterval = 5;
+        int tickCount = 1;
+
+        //
+        [SerializeField] LayerMask cubeFilter;
+        [SerializeField] LayerMask ground;
+
+        private void Start()
+        {
+            playerCam = camMan.playerCamera;
+        }
+
+        private void FixedUpdate()
+        {
+            // Tick system to not blow up the computer.
+            tickCount++;
+            if (tickCount >= raycastInterval)
+            {
+                tickCount = 1;
+
+                float distance = defaultDistance;
+                // raycast and draw ray. DO NOT DO THIS EVERY FRAME! I'M JUST SILLY!
+                if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out RaycastHit hit, 10, ground.value))
+                {
+                    distance = hit.distance;
+                }
+                Debug.Log($"Distance is {distance}");
+
+                // Debug.
+                Vector3 camPos = playerCam.transform.position;
+                Debug.DrawLine(camPos, camPos + playerCam.transform.forward * 10);
+
+                RaycastHit[] hits = Physics.RaycastAll(playerCam.transform.position, playerCam.transform.forward, distance, cubeFilter.value);
+                foreach (RaycastHit hit2 in hits)
+                {
+                    if (hit2.collider.TryGetComponent(out Renderer renderer))
+                    {
+                        renderer.material.color = Color.red;
+                    }
+                }
+                Debug.Log($"Hit {hits.Length} cubes!");
+            }
+            Debug.Log($"Ticknum = {tickCount}.");
+        }
+    }
+}
